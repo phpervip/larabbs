faf148b383ebac84f51541fb3180e416356566a6
phpervip <2998658517@qq.com>
2019-07-31 06:39:41 +0000

    生成话题骨架

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -12,7 +12,6 @@ class UsersController extends Controller
     public function __construct()
     {
        $this->middleware('auth',['except'=>['show']]);
-        // $this->authorize('');
     }
 
 

d4f71ba67044ba5e24ef8208303a5f2e5f3e28c1
phpervip <2998658517@qq.com>
2019-07-31 07:00:19 +0000

    填充用户和话题数据

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,8 +20,10 @@ $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
+        'remember_token'=>Str::random(10),
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // password
+        'introduction'=>$faker->sentence(),
         'remember_token' => Str::random(10),
     ];
 });

d211632d04104efd4842ea65a34fee0edb02de60
phpervip <2998658517@qq.com>
2019-08-01 05:46:13 +0000

    翻译 URL 标示

--- config/services.php
+++ config/services.php
@@ -44,4 +44,11 @@ return [
         ],
     ],
 
+
+    'baidu_translate' => [
+        'appid' => env('BAIDU_TRANSLATE_APPID'),
+        'key'=>env('BAIDU_TRANSLATE_KEY')
+    ],
+
+
 ];
--- routes/web.php
+++ routes/web.php
@@ -35,12 +35,14 @@ Route::resource('users','UsersController',['only'=>['show','update','edit']]);
 
 
 
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
 
 Route::resource('categories','CategoriesController',['only'=>['show']]);
 
 
 Route::any('upload_image','TopicsController@uploadImage')->name('topics.upload_image');
+
+Route::get('topics/{topic}/{slug?}','TopicsController@show')->name('topics.show');
 
 
 

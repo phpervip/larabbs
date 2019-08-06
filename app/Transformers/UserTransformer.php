b0cd6ce70205ee5788c45227340ebeeed8db5019
phpervip <2998658517@qq.com>
2019-08-06 06:20:38 +0000

    include user roles

--- app/Transformers/UserTransformer.php
+++ app/Transformers/UserTransformer.php
@@ -7,6 +7,10 @@ use League\Fractal\TransformerAbstract;
 
 class UserTransformer extends TransformerAbstract
 {
+
+    protected $availableIncludes = ['roles'];
+
+
     public function transform(User $user)
     {
         return [
@@ -21,5 +25,10 @@ class UserTransformer extends TransformerAbstract
             'created_at' => (string) $user->created_at,
             'updated_at' => (string) $user->updated_at,
         ];
+    }
+
+    public function includeRoles(User $user)
+    {
+        return $this->collection($user->roles, new RoleTransformer());
     }
 }

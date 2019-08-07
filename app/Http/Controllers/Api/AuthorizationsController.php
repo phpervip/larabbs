3500948325dd752034ee7c20274d86bceaaeaf01
phpervip <2998658517@qq.com>
2019-08-07 06:20:32 +0000

    add easywechat

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -6,12 +6,32 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\Api\SocialAuthorizationRequest;
 use App\Http\Requests\Api\AuthorizationRequest;
+use App\Http\Requests\Api\WeappAuthorizationRequest;
 use Auth;
+
+use Psr\Http\Message\ServerRequestInterface;
+use League\OAuth2\Server\AuthorizationServer;
+use Zend\Diactoros\Response as Psr7Response;
+use League\OAuth2\Server\Exception\OAuthServerException;
+
+use App\Traits\PassportToken;
 
 class AuthorizationsController extends Controller
 {
 
-     public function store(AuthorizationRequest $request){
+     use PassportToken;
+
+     public function store(AuthorizationRequest $originRequest, AuthorizationServer $server, ServerRequestInterface $serverRequest){
+
+
+        try {
+            return $server->respondToAccessTokenRequest($serverRequest, new Psr7Response)->withStatus(201);
+        } catch(OAuthServerException $e) {
+            return $this->response->errorUnauthorized($e->getMessage());
+        }
+
+
+
         $username = $request->username;
 
         filter_var($username, FILTER_VALIDATE_EMAIL)?
@@ -31,6 +51,8 @@ class AuthorizationsController extends Controller
 
      public function socialStore($type, SocialAuthorizationRequest $request)
     {
+
+
         if (!in_array($type, ['weixin'])) {
             return $this->response->errorBadRequest();
         }
@@ -77,8 +99,11 @@ class AuthorizationsController extends Controller
                 break;
         }
 
-        $token = \Auth::guard('api')->fromUser($user);
-        return $this->respondWithToken($token);
+        // $token = \Auth::guard('api')->fromUser($user);
+        // return $this->respondWithToken($token);
+
+        $result = $this->getBearerTokenByUser($user, '1', false);
+        return $this->response->array($result)->setStatusCode(201);
     }
 
 
@@ -93,15 +118,105 @@ class AuthorizationsController extends Controller
     }
 
 
-    public function update()
+    // public function update(AuthorizationServer $server, ServerRequestInterface $serverRequest)
+    // {
+    //     // $token = Auth::guard('api')->refresh();
+    //     // return $this->respondWithToken($token);
+
+    //     try {
+    //        return $server->respondToAccessTokenRequest($serverRequest, new Psr7Response);
+    //     } catch(OAuthServerException $e) {
+    //         return $this->response->errorUnauthorized($e->getMessage());
+    //     }
+    // }
+
+
+     public function update(AuthorizationServer $server, ServerRequestInterface $serverRequest)
     {
-        $token = Auth::guard('api')->refresh();
-        return $this->respondWithToken($token);
+        try {
+           return $server->respondToAccessTokenRequest($serverRequest, new Psr7Response);
+        } catch(OAuthServerException $e) {
+            return $this->response->errorUnauthorized($e->getMessage());
+        }
     }
+
+
 
     public function destroy()
     {
-        Auth::guard('api')->logout();
-        return $this->response->noContent();
+        // Auth::guard('api')->logout();
+        // return $this->response->noContent();
+
+        if (!empty($this->user())) {
+            $this->user()->token()->revoke();
+            return $this->response->noContent();
+        } else {
+            return $this->response->errorUnauthorized('The token is invalid.');
+        }
     }
+
+    public function weappStore(WeappAuthorizationRequest $request)
+    {
+        $code = $request->code;
+
+        // 根据 code 获取微信 openid 和 session_key
+        $miniProgram = \EasyWeChat::miniProgram();
+        $data = $miniProgram->auth->session($code);
+
+        // 如果结果错误，说明 code 已过期或不正确，返回 401 错误
+        if (isset($data['errcode'])) {
+            return $this->response->errorUnauthorized('code 不正确');
+        }
+
+        // 找到 openid 对应的用户
+        $user = User::where('weapp_openid', $data['openid'])->first();
+
+        $attributes['weixin_session_key'] = $data['session_key'];
+
+        // 未找到对应用户则需要提交用户名密码进行用户绑定
+        if (!$user) {
+            // 如果未提交用户名密码，403 错误提示
+            if (!$request->username) {
+                return $this->response->errorForbidden('用户不存在');
+            }
+
+            $username = $request->username;
+
+            // 用户名可以是邮箱或电话
+            filter_var($username, FILTER_VALIDATE_EMAIL) ?
+                $credentials['email'] = $username :
+                $credentials['phone'] = $username;
+
+            $credentials['password'] = $request->password;
+
+            // 验证用户名和密码是否正确
+            if (!Auth::guard('api')->once($credentials)) {
+                return $this->response->errorUnauthorized('用户名或密码错误');
+            }
+
+            // 获取对应的用户
+            $user = Auth::guard('api')->getUser();
+            $attributes['weapp_openid'] = $data['openid'];
+        }
+
+        // 更新用户数据
+        $user->update($attributes);
+
+        // 为对应用户创建 JWT
+        $token = Auth::guard('api')->fromUser($user);
+
+        return $this->respondWithToken($token)->setStatusCode(201);
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
+

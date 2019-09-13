a11fba8de5a5040d5a476fa9de204e31c495eee7
phper <2998658517@qq.com>
2019-09-13 04:51:32 +0000

    修改footer

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -5,6 +5,6 @@
         由<a href="http://weibo.com/u/1837553744?is_hot=1">Yyii</a>设计和编码
 <span style="color: #e27575;font-size: 14px;">❤</span>
     </p>
-    <p class="float-right"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
+    <p class="float-right"><a href="http://beauty.yyii.info">亿莲科技.技术支持</a></p>
   </div>
 </footer>
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
   <div class="container">
-    <a class="navbar-brand" href="/">LaraBBS</a>
+    <a class="navbar-brand" href="/">佛系论坛</a>
     <!-- <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggle-icon"></span>
     </button> -->
